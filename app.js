537d661b47c8302ab5b723a98c25dcc59c4bd40a
lochinbek_9 <lochinbeksetor@gmail.com>
2023-12-13 10:34:42 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+
+
+const boxBtn = document.querySelector(".box-btn");
+
+const text = document.querySelector(".text");
+
+const resetBtn = document.querySelector(".reset");
+
+const score = document.querySelector("h2");
+
+const all = document.querySelector("h4");
+
+let audio = new Audio('ovoz.mp3');
+
+let a = 0;
+let b = 0;
+let c = 1
+
+boxBtn.addEventListener("click", () =>{
+    a++;
+    
+    all.textContent = 'jami: ' +c 
+    c++ 
+audio.play();
+
+
+    
+    text.textContent = a+'/33' ;
+    
+    if(a===33){
+        a = 0;
+    text.textContent = 0;
+    
+    score.textContent = ++b;
+    }
+
+});
+
+resetBtn.addEventListener("click", () =>{
+    a = 0;
+    text.textContent = 0;
+});
